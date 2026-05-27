78a067b17db90762313a344a5677026e012ff497
valeponick <vale.ponick@gmail.com>
2026-05-27 09:04:36 +0700

    "✨ feat: add DiskFree with error handling"

--- ValeNetUtils/main.swift
+++ ValeNetUtils/main.swift
@@ -7,5 +7,57 @@
 
 import Foundation
 
-print("Hello, World!")
+print("DiskFree: Показывает свободное место на диске в читаемом формате")
 
+let arguments = CommandLine.arguments // это массив строк -> получаем аргументы командной строки
+
+guard arguments.count > 1 else {
+    print("❌ Show a path: diskfree /")
+    exit(1)
+}
+let discPath = arguments[1]
+
+let process = Process()
+let pipe = Pipe()
+
+process.executableURL = URL(fileURLWithPath: "/bin/df")
+process.arguments = ["-h", discPath]
+process.standardOutput = pipe
+process.standardError = pipe
+
+do {
+    try process.run()
+    process.waitUntilExit()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8) ?? ""
+    
+    if output.contains("No such file") {
+        print("❌ Puth not exist")
+    } else {
+        let lines = output.split(separator: "\n")
+        
+        if lines.count > 1 {
+                    let dataLine = lines[1]
+                    let columns = dataLine.split(separator: " ")
+                    
+                    if columns.count > 3 {
+                        let available = columns[3]
+                        print("✅ \(discPath): \(available) free")
+                    }
+                }
+            }
+            
+        } catch {
+            print("Error: \(error)")
+    }
+// DiskFree: Показывает свободное место на диске в читаемом формате
+// ❌ Puth not exist
+    
+ /* написала полезную утилиту, которая:
+    1 .Проверяет аргументы
+    2. Запускает системную команду
+    3. Парсит вывод
+    4. Обрабатывает ошибки
+    5. Выводит красивый результат */
+  
